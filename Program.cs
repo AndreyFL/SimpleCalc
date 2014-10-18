2b64cf3ba15c105bf7860602daca8006921ebb53
AndreyFL <AndreyFilchenkov@gmail.com>
2014-10-18 23:11:43 +0300

    20141018 Some improvements.
    I`ve added few checking algorithms.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 
 namespace SimpleCalc
 {
@@ -13,6 +13,7 @@ namespace SimpleCalc
             //string inputString = "";
             byte countBracketsStart = 0;
             byte countBracketsEnd = 0;
+            const string allowedSymbols = "0123456789+-*/()";
 
             Console.WriteLine("Введите строку для вычисления:");
             inputString = Console.ReadLine();
@@ -27,45 +28,27 @@ namespace SimpleCalc
                 inputString = inputString.Substring(0, inputString.IndexOf('='));
 
             // Проверяю количество скобок. Кол-во открывающих должно равняться количеству закрывающих
+
             for (int i = 0; i < inputString.Length; i++)
             {
                 if (inputString[i] == '(')
                     countBracketsStart++;
                 else if (inputString[i] == ')')
                     countBracketsEnd++;
-                switch (inputString[i])// Проверяю наличие ошибочных символов
-                {
-                    case '0':
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    case '*':
-                    case '/':
-                    case '+':
-                    case '-':
-                    case '(':
-                    case ')':
-                        break;
-                    default:// дефолт, если в выражении присутствуют ошибочные символы.
-                        return false;
-                }
 
+                // Проверяю наличие ошибочных символов, если они присутствуют возвращаю из метода FALSE.
+                if (!allowedSymbols.Contains(inputString[i]))
+                    return false;
+
+                // Начинаться и оканчиваться строка может только на цифру или скобку, в других случаях ошибка
+                if (!IsCharsDigitsOrBrackets(inputString[0], inputString[inputString.Length - 1]))
+                    return false;
             }
             // Если количество открывающих не равно кол-ву закрываемых, возвращаю признак неуспешности.
             if (countBracketsStart != countBracketsEnd)
                 return false;
             return true;
         }
-
-
-
-
 
 
         static int DoSomeCalculation(string inputString)
@@ -84,7 +67,7 @@ namespace SimpleCalc
                 // не раскроются все скобки. После этого рекурсивный метод возвратит численное значение (результат) 
                 // вычисления выражения во всех скобках. Подставляю этот результат во входную строку вместо выражения в скобках.
 
-                
+
 
                 int tmpInt;
                 string tmpStr;
@@ -108,22 +91,18 @@ namespace SimpleCalc
                     }
                 }
 
-                //indexOfEndBracket = (ushort)inputString.LastIndexOf(')');
-
                 strInBrackets = inputString.Substring(indexOfStartBracket + 1, indexOfEndBracket - indexOfStartBracket - 1);
-                // Выражение в скобках должно начинаться и оканчиваться на скобку или цифру, если по другому- ошибка, генерирую исключение.
-                if (!IsCharsDigitsOrBrackets((char)strInBrackets[0], (char)strInBrackets[strInBrackets.Length - 1]))
+                // Выражение в скобках должно начинаться и оканчиваться на скобку или цифру, 
+                // если по другому- ошибка, генерирую исключение, также производится проверка на то, чтобы длинна передаваемой строки
+                // не равнялась 0.
+                if (indexOfStartBracket == indexOfEndBracket + 1 ||
+                    (!IsCharsDigitsOrBrackets((char)strInBrackets[0], (char)strInBrackets[strInBrackets.Length - 1])))
                     throw new Exception();
                 // Вызываю метод рекурсивно с новой строкой.
                 tmpInt = DoSomeCalculation(strInBrackets);
                 tmpStr = inputString.Substring(0, indexOfStartBracket) + tmpInt.ToString() + inputString.Substring(indexOfEndBracket + 1, inputString.Length - 1 - indexOfEndBracket);
                 inputString = tmpStr;
             }
-
-                
-
-
-
 
 
             // На данном этапе входная строка содержит только линейные выражения без скобок, т.е. операнды и 
@@ -192,13 +171,7 @@ namespace SimpleCalc
         }
 
 
-        /// <summary>
-        /// Выполняет элементарные арифметические действия
-        /// </summary>
-        /// <param name="opr"></param>
-        /// <param name="leftOperand"></param>
-        /// <param name="rightOperand"></param>
-        /// <returns></returns>
+        // Выполняет элементарные арифметические действия и возвращает результат
         static int DoArifmeticActions(char opr, int leftOperand, int rightOperand)
         {
             int result;
@@ -218,14 +191,18 @@ namespace SimpleCalc
         }
 
 
+        // Возвращает сообщение об ошибке, и завершает программу.
         static void ShowErrorMessageAndExit()
         {
             Console.WriteLine("Ошибочное выражение!");
+            Console.WriteLine("Для продолжения нажмите любую клавишу!");
+            Console.ReadKey();
             Environment.Exit(-1);
 
         }
 
-        // Этот метод проверяет является ли переданный символ скобкой или 
+        // Этот метод проверяет является ли переданные символы открывающими или закрывающими скобками или 
+        // числами. Если да, возвращает True.
         static bool IsCharsDigitsOrBrackets(char firstChar, char lastChar)
         {
             if (firstChar != '(' && ((int)firstChar < 48 || (int)firstChar > 57) ||
@@ -241,21 +218,17 @@ namespace SimpleCalc
             string inputString;
             // GetInputString(out inputString) возвращает true или false в зависимости от успешности
             // получения входной строки, и через *out* возвращает полученную строку.
-            if (GetInputString(out inputString))
-            {
-                //Console.WriteLine("\n" + inputString);
-            }
-            else
-            {
+            if (!GetInputString(out inputString))
                 ShowErrorMessageAndExit();
-            }
 
             // DoSomeCalculation(inputString); возвращает интовое значение результата, 
             // и в случае неправильного расставления скобок или арифметических операторов
             // сгенерирует исключение.
             try
             {
-                Console.WriteLine(DoSomeCalculation(inputString));
+                Console.WriteLine("\nРезультат:\n{0}={1}", inputString, DoSomeCalculation(inputString));
+                Console.WriteLine("Для продолжения нажмите любую клавишу!");
+                Console.ReadKey();
             }
             catch
             {
